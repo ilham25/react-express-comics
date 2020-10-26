335f20be3e97d04b50742430206cee8bcd358979
ilham25 <ilhamadiputra4@gmail.com>
2020-10-26 07:44:21 +0700

    add simple interface

--- server/index.js
+++ server/index.js
@@ -1,7 +1,16 @@
 import Express from "express";
+import BodyParser from "body-parser";
+import Cors from "cors";
+import Mysql from "mysql";
 
 const app = Express();
 const port = 3030;
+
+app.post("/api/insert", (req, res) => {});
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
 
 app.listen(port, () => {
   console.log(`Server run on http://localhost:${port}`);
